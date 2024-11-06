af08ce523b976215567d0988c5a5d78680217e60
J. Enrique Agudo <jeagudo@unex.es>
2024-11-06 20:24:09 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,14 @@
+namespace hello {
+    //% block
+    export function helloWorld() {
+
+    }
+
+    // note that Caml casing yields lower case
+    // block text with spaces
+
+    //% block
+    export function camlCaseTwo() {
+
+    }
+}
